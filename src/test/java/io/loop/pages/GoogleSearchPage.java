06246f6ee2f25987ae6d18c08620a3d3fe143676
nargiz <nargiz.d.aslanova@gmail.com>
2025-02-23 09:28:13 -0500

    cucumber framewor

--- src/test/java/io/loop/pages/GoogleSearchPage.java
+++ src/test/java/io/loop/pages/GoogleSearchPage.java
@@ -1,9 +1,12 @@
 package io.loop.pages;
 
 import io.loop.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleSearchPage {
 
@@ -19,4 +22,23 @@ public class GoogleSearchPage {
 
     @FindBy(xpath = "//div[@class='recaptcha-checkbox-border']" )
     public WebElement captcha;
+
+    @FindBy(xpath = "//a[@class='C3TEmf']")
+    public WebElement capital;
+
+    public void handleReCaptcha(WebDriver driver, WebElement captchaElement) {
+
+        try {
+            WebElement iframe = driver.findElement(By.xpath("//iframe[@title='reCAPTCHA']"));
+            driver.switchTo().frame(iframe);
+
+            if (captchaElement.isDisplayed()) {
+                captchaElement.click();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        driver.switchTo().defaultContent();
+
+    }
 }
